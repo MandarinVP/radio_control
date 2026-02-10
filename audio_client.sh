e64dc07932866b4b85efd2bac11062e69c0b058e
MandarinVP <mandarinvp@yandex.ru>
2026-02-10 20:06:57 +0500

    Update audio_client.sh

--- audio_client.sh
+++ audio_client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gst-launch-1.0   udpsrc port=5000 buffer-size=65536 caps="application/x-rtp,payload=96,clock-rate=48000,encoding-name=OPUS" !   rtpjitterbuffer latency=100 drop-on-latency=false do-lost=false !   queue max-size-time=50000000 leaky=downstream !   rtpopusdepay !   opusdec plc=false use-inband-fec=true !   queue !   audioconvert !   queue !   alsasink device=hw:0 buffer-time=100000 latency-time=1000 sync=false
+gst-launch-1.0 udpsrc port=5000 buffer-size=65536 caps="application/x-rtp,payload=96,clock-rate=48000,encoding-name=OPUS" ! rtpjitterbuffer latency=100 drop-on-latency=false do-lost=false ! queue max-size-time=50000000 leaky=downstream ! rtpopusdepay ! opusdec plc=false use-inband-fec=true ! queue ! audioconvert ! queue ! pulsesink buffer-time=100000 latency-time=1000
 
